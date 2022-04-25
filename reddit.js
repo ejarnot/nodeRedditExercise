bd70c8909d98f6bf6ddf4320846fa217085c7f91
Mia Jarnot <ejarnot42@outlook.com>
2022-04-25 08:56:07 -0700

    fixed syntax errors and other bugs

--- reddit.js
+++ reddit.js
@@ -1,20 +1,22 @@
 const fetch = require("isomorphic-fetch")
 const { join } = require("path")
-const fs = require("fs")
+const { writeFile } = require("fs")
 
 const dataPath = join(__dirname, "./popular-articles.json")
 
-fetch("https://reddit.com/r/popular.json", (err, body) => {
-    if(err) console.log(err);
-    let articles = []
-
-    JSON.parse(body).data.childre.forEach(item => {
-        articles.push({
-            title: item.data.title,
-            url: item.data.url,
-            author: item.data.author
+fetch("https://reddit.com/r/programmingHumor.json")
+    .then((res) => res.json())
+    .then(({data: { children }}) =>{
+        let articles =  children.map(({ data: { url, author, title } }) => {
+            return { url, author, title };
         });
+
+    
+        writeFile(dataPath, JSON.stringify(articles), (err) => {
+            if (err) {
+            console.error(err);
+            } else {
+            console.log("Successfully wrote to file.");
+            }
+         });
     })
-    console.log(articles)
-    fs.writeFile(dataPath, JSON.stringify(articles))
-})
